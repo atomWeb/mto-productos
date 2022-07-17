90a600914f5531ba164fe3c4c72056ddf1e85d8a
atomWeb <cristianjose@gmail.com>
2022-07-17 12:47:50 +0200

    Cap 23 HTTP requests

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
-import { StaticDataSource } from './static.datasource';
+// import { StaticDataSource } from './static.datasource';
 import { Model } from './repository.model';
+// import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { RestDataSource, REST_URL } from './rest.datasource';
 
 @NgModule({
-  providers: [Model, StaticDataSource],
+  // imports: [HttpClientModule, HttpClientJsonpModule],
+  imports: [HttpClientModule],
+  providers: [
+    Model,
+    // StaticDataSource,
+    RestDataSource,
+    {
+      provide: REST_URL,
+      useValue: `http://${location.hostname}:3500/products`,
+    },
+  ],
 })
 export class ModelModule {}
+
+// JSONP can be used only to make GET requests, and it presents greater security risks
+// than CORS. As a consequence, JSONP should be used only when CORS isn’t available.
+// The Angular support for JSONP is defined in a feature module called:
+// HttpClientJsonpModule,
--- src/app/model/repository.model.ts
+++ src/app/model/repository.model.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Product } from './product.model';
 import { StaticDataSource } from './static.datasource';
+import { Observable } from 'rxjs';
+import { RestDataSource } from './rest.datasource';
 
 @Injectable()
 export class Model {
   private products: Product[];
   private locator = (p: Product, id?: number) => p.id == id;
-  constructor(private dataSource: StaticDataSource) {
+
+  // constructor(private dataSource: StaticDataSource) {
+  //   this.products = new Array<Product>();
+  //   this.dataSource.getData().forEach((p) => this.products.push(p));
+  // }
+
+  constructor(private dataSource: RestDataSource) {
     this.products = new Array<Product>();
-    this.dataSource.getData().forEach((p) => this.products.push(p));
+    this.dataSource.getData().subscribe((data) => (this.products = data));
   }
 
   getProducts(): Product[] {
@@ -19,28 +27,48 @@ export class Model {
     return this.products.find((p) => this.locator(p, id));
   }
 
+  // saveProduct(product: Product) {
+  //   if (product.id == 0 || product.id == null) {
+  //     product.id = this.generateID();
+  //     this.products.push(product);
+  //   } else {
+  //     let index = this.products.findIndex((p) => this.locator(p, product.id));
+  //     this.products.splice(index, 1, product);
+  //   }
+  // }
   saveProduct(product: Product) {
     if (product.id == 0 || product.id == null) {
-      product.id = this.generateID();
-      this.products.push(product);
+      this.dataSource
+        .saveProduct(product)
+        .subscribe((p) => this.products.push(p));
     } else {
-      let index = this.products.findIndex((p) => this.locator(p, product.id));
-      this.products.splice(index, 1, product);
+      this.dataSource.updateProduct(product).subscribe((p) => {
+        let index = this.products.findIndex((item) => this.locator(item, p.id));
+        this.products.splice(index, 1, p);
+      });
     }
   }
+  // deleteProduct(id: number) {
+  //   let index = this.products.findIndex((p) => this.locator(p, id));
+  //   if (index > -1) {
+  //     this.products.splice(index, 1);
+  //   }
+  // }
 
   deleteProduct(id: number) {
-    let index = this.products.findIndex((p) => this.locator(p, id));
-    if (index > -1) {
-      this.products.splice(index, 1);
-    }
+    this.dataSource.deleteProduct(id).subscribe(() => {
+      let index = this.products.findIndex((p) => this.locator(p, id));
+      if (index > -1) {
+        this.products.splice(index, 1);
+      }
+    });
   }
-  
-  private generateID(): number {
-    let candidate = 100;
-    while (this.getProduct(candidate) != null) {
-      candidate++;
-    }
-    return candidate;
-  }
+
+  // private generateID(): number {
+  //   let candidate = 100;
+  //   while (this.getProduct(candidate) != null) {
+  //     candidate++;
+  //   }
+  //   return candidate;
+  // }
 }
